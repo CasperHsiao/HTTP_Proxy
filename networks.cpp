d62bdeef10583aa01b25c05909c8ba5a773dc2d5
Casper Hsiao <casperhpw@gmail.com>
2022-02-17 13:49:41 -0500

    dev: implement handle_connect_request method in networks

--- networks.cpp
+++ networks.cpp
@@ -76,7 +76,7 @@ void listen_for_connections(int listener_fd) {  // ONLY accept once for now
   while (true) {
     int poll_count = poll(pfds, 1, -1);
     if (poll_count == -1) {
-      std::cerr << "Error: poll_count error" << std::endl;
+      std::cerr << "Error: poll_count accept error" << std::endl;
       throw std::exception();
     }
 
@@ -93,4 +93,55 @@ void listen_for_connections(int listener_fd) {  // ONLY accept once for now
     //return new_fd;
   }
   delete pfds;
+}
+
+void handle_connect_request(int client_fd, int server_fd, Request & request) {
+  char msg[] = "HTTP/1.1 200 OK\r\n\r\n";
+  int nbytes;
+  if ((nbytes = send(client_fd, msg, strlen(msg) + 1, 0)) ==
+      -1) {  // +1 accounts null terminator
+    std::cerr << "Error: failed to send connect request success to client" << std::endl;
+    throw std::exception();
+  }
+  struct pollfd * pfds_recv = new struct pollfd[2];
+  struct pollfd * pfds_send = new struct pollfd[2];
+  // recv() from pfds_recv[i].fd just send() to pfds_send[i].fd
+  pfds_recv[0].fd = client_fd;
+  pfds_recv[0].events = POLLIN;
+  pfds_send[0].fd = server_fd;
+  pfds_send[0].events = POLLOUT;
+  pfds_recv[1].fd = server_fd;
+  pfds_recv[1].events = POLLIN;
+  pfds_send[1].fd = client_fd;
+  pfds_send[1].events = POLLOUT;
+  while (true) {
+    int poll_count_recv = poll(pfds_recv, 2, -1);
+    if (poll_count_recv == -1) {
+      std::cerr << "Error: poll_count_recv" << std::endl;
+      throw std::exception();
+    }
+
+    for (int i = 0; i < 2; i++) {
+      if (pfds_recv[i].revents & POLLIN) {
+        int nbytes;
+        char buf[65536];
+        if ((nbytes = recv(pfds_recv[i].fd, buf, sizeof(buf), 0)) <= 0) {
+          std::cerr << "Error: failed to receive from connection tunnel" << std::endl;
+          throw std::exception();
+        }
+        while (true) {
+          int poll_count_send = poll(&pfds_send[i], 1, -1);
+          if (poll_count_send == -1) {
+            std::cerr << "Error: poll_count_send" << std::endl;
+            throw std::exception();
+          }
+          if ((nbytes = send(pfds_send[i].fd, buf, nbytes, 0)) == -1) {
+            std::cerr << "Error: failed to send to connection tunnel" << std::endl;
+            throw std::exception();
+          }
+          break;
+        }
+      }
+    }
+  }
 }
