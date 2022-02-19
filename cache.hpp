f6e8066eea053afb3a1cc0cac6ebf58c95254e3c
Harrison Hao-Yu Ku <hk261@duke.edu>
2022-02-19 03:40:25 -0500

    Finish no-cache

--- cache.hpp
+++ cache.hpp
@@ -6,17 +6,11 @@
 #include <unordered_map>
 #include <vector>
 
-<<<<<<< HEAD
 #include "response.hpp"
 
 class Cache {
  public:
   std::unordered_map<std::string, Response> cache_data;
-=======
-class Cache {
- public:
-  std::unordered_map<std::string, std::string> cache_data;
->>>>>>> 967bcffcb4add9e7b40b2383811f0f86868b433d
   std::vector<std::string> cache_saved_order;
 };
 
